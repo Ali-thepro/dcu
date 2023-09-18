5b3f9b1c11c36adb9da6fc516526ecf4fcd2ecbb
Ali-thepro <aliahmad2005yt@gmail.com>
2023-09-18 14:10:21 +0100

    ca268

--- ca268/lab-1/q3.py
+++ ca268/lab-1/q3.py
@@ -1,16 +1,40 @@
 class Memories:
     def __init__(self, **attributes):
-        self.attributes = attributes
+        for k, v in attributes.items():
+            setattr(self, k, v)
 
     def remember(self, attribute):
-        if attribute in self.attributes:
-            return self.attributes[attribute]
+        if attribute in self.__dict__:
+            return self.__dict__[attribute]
         else:
             return False
+        
+    # def remember(self, attribute):
+    #     if hasattr(self, attribute):
+    #         return getattr(self, attribute)
+    #     else:
+    #         return False
 
 person1 = Memories(name='Tom', age=32, salary=50000)
+print(person1.__dict__) 
 print(person1.remember('salary')) 
 print(person1.remember('email')) 
+
+
+
+# class Memories:
+#     def __init__(self, **attributes):
+#         self.attributes = attributes
+
+#     def remember(self, attribute):
+#         if attribute in self.attributes:
+#             return self.attributes[attribute]
+#         else:
+#             return False
+
+# person1 = Memories(name='Tom', age=32, salary=50000)
+# print(person1.remember('salary')) 
+# print(person1.remember('email')) 
 
 
 
