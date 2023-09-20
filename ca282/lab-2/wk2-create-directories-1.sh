e846a52992a7c644253a4915a0ac09162babbe30
Ali-thepro <aliahmad2005yt@gmail.com>
2023-09-20 22:28:26 +0100

    ca282/lab-2

--- ca282/lab-2/wk2-create-directories-1.sh
+++ ca282/lab-2/wk2-create-directories-1.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-n="$1"
-i=0
-while [ "$i" -lt "$n" ]
-do
-    mkdir "dir."$((i+=1))
-done
+# n="$1"
+# i=0
+# while [ "$i" -lt "$n" ]
+# do
+#     mkdir "dir."$((i+=1))
+# done
 
 
 # i=1
@@ -14,3 +14,11 @@ done
 #     mkdir "dir.$i"
 #     i=$((i+1))
 # done
+
+
+
+# n="$1"
+# seq -f "dir.%.0f" $n | xargs mkdir
+
+# n="$1"
+# mkdir $(seq -f "dir.%.0f" $n)
--- ca282/lab-2/wk2-create-directories-2.sh
+++ ca282/lab-2/wk2-create-directories-2.sh
@@ -8,3 +8,17 @@ do
     mkdir "$fileName"
     i=$((i+=1))
 done
+
+# n="$1"
+# i=1
+# while [ "$i" -le "$n" ]
+# do
+#     fileName=$(printf "%06d\n" "$i")
+#     mkdir "dir.$fileName"
+#     i=$((i+=1))
+# done
+
+
+
+# n="$1"
+# seq -f "dir.%06.0f" $n | xargs mkdir
