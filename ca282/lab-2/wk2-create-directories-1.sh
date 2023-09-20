97136c4ef70f0b0c061a407ffbde1a49d2eba1c8
Ali-thepro <aliahmad2005yt@gmail.com>
2023-09-20 22:29:48 +0100

    ca282/lab-2

--- ca282/lab-2/wk2-create-directories-1.sh
+++ ca282/lab-2/wk2-create-directories-1.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# n="$1"
-# i=0
-# while [ "$i" -lt "$n" ]
-# do
-#     mkdir "dir."$((i+=1))
-# done
+n="$1"
+i=0
+while [ "$i" -lt "$n" ]
+do
+    mkdir "dir."$((i+=1))
+done
 
 
 # i=1
